979da2172235e3ea7cf6fe2c9eb678d870fd11cd
Jayden <jaydenhthompson@gmail.com>
2020-11-24 09:06:34 -0500

    Added google sign in

--- app/src/main/java/edu/utap/dotsandboxes/MainActivity.kt
+++ app/src/main/java/edu/utap/dotsandboxes/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.utap.dotsandboxes
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
@@ -12,6 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
         toolbarText.text = "DOTS & BOXES"
+
+        val provider = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(provider)
+                .build(),
+            1
+        )
 
         setListeners()
     }
